a6a2deaa46b65092128b0ba33245153626bba03f
dishin <dishin@maitzsoft.co.kr>
2024-11-22 17:51:17 +0900

    programmers

--- BankingCountStatement/bin/test.js
+++ BankingCountStatement/bin/test.js
@@ -134,72 +134,252 @@
 
 // console.log(summarizeCart(cart));
 
-function getBill(item) {
-  return {
-    name: item.name,
-    due: twoWeeksFromNow(),
-    total: calculateTotal(item.price),
-  };
-}
+// function getBill(item) {
+//   return {
+//     name: item.name,
+//     due: twoWeeksFromNow(),
+//     total: calculateTotal(item.price),
+//   };
+// }
 
-const bill = getBill({
-  name: 'Room Cleaning',
-  price: 30,
-});
+// const bill = getBill({
+//   name: 'Room Cleaning',
+//   price: 30,
+// });
 
-function twoWeeksFromNow() {
-  const today = new Date();
-  const numberOfDays = 14;
-  today.setDate(today.getDate() + numberOfDays);
-  return today.toDateString();
-}
+// function twoWeeksFromNow() {
+//   const today = new Date();
+//   const numberOfDays = 14;
+//   today.setDate(today.getDate() + numberOfDays);
+//   return today.toDateString();
+// }
 
-function calculateTotal(price) {
-  const taxRate = 0.1;
-  return price + (price * taxRate);
-}
+// function calculateTotal(price) {
+//   const taxRate = 0.1;
+//   return price + (price * taxRate);
+// }
 
-function displayBill(bill) {
-  return `The ${bill.name} bill is ${bill.total} and is due on ${bill.due}`;
-}
+// function displayBill(bill) {
+//   return `The ${bill.name} bill is ${bill.total} and is due on ${bill.due}`;
+// }
 
-console.log(displayBill(bill));
+// console.log(displayBill(bill));
 
-const defaults = {
-  author: '',
-  title: '',
-  year: 2017,
-  rating: null,
+// const defaults = {
+//   author: '',
+//   title: '',
+//   year: 2017,
+//   rating: null,
+// };
+
+// const book = {
+//   author: 'Joe Morgan',
+//   title: 'Simplifying JavaScript',
+// };
+
+// function addBookDefaults(book, defaults) {
+//   const fields = Object.keys(defaults);
+//   // const bookFields = Object.keys(book);
+//   // console.log(fields);
+//   // console.log(bookFields);
+
+//   const updated = {};
+
+//   for (let i = 0; i < fields.length; i++) {
+//     const field = fields[i];
+//     updated[field] = book[field] || defaults[field];
+//   }
+// }
+
+// addBookDefaults(book, defaults);
+
+// let filters = new Map(
+//   [
+//     ['author', ''],
+//     ['title', ''],
+//     ['year', 2017],
+//     ['rating', null],
+//   ]
+// );
+
+// console.log(filters);
+
+// const filters = new Map().set('색상', '검정색')
+//                          .set('견종', '래브라도레트리버');
+
+// function checkFilters(filters) {
+//   for (const entry of filters) {
+//     console.log(entry);
+//   }
+// }
+
+// // checkFilters(filters);
+
+// filters.entries();
+
+// const dogs = [
+//   { 이름: '맥스', 크기: '소형견', 견종: '보스턴테리어', 색상: '검정색' },
+//   { 이름: '도니', 크기: '대형견', 견종: '래브라도레트리버', 색상: '검정색' },
+//   { 이름: '섀도', 크기: '중형견', 견종: '래브라도레트리버', 색상: '갈색' },
+// ];
+
+// function getColors(dogs) {
+//   return dogs.map(dog => dog['색상']);
+// }
+
+// console.log(getColors(dogs));
+
+// function getColorsFilter(attr) {
+//   const unique = [];
+
+//   for (const check of attr) {
+//     if (!unique.includes(check)) {
+//       unique.push(check);
+//     }
+//   }
+
+//   return unique;
+// }
+
+// console.log(getColorsFilter(getColors(dogs)));
+
+// function solution(n, t) {
+//   var answer = n;
+//   for (let i = 0; i < t; i++) {
+//     answer = answer * 2;
+//   }
+//   return answer;
+// }
+
+// console.log(solution(2, 10));
+
+// function solution(my_string) {
+//   var answer = '';
+//   var strArr = my_string.split('');
+//   var size = strArr.length - 1;
+//   for (var i = 0; i < strArr.length; i++) {
+//     answer += strArr[size];
+//     size--;
+//   }
+
+//   return answer;
+// }
+
+// console.log(solution('world'));
+
+// function solution(my_string) {
+//   var answer = []; // Initialize an array
+
+//   for (var i = my_string.length - 1; i >= 0; i--) {
+//     answer.push(my_string[i]); // Push characters in reverse order
+//   }
+
+//   return answer.join(''); // Convert array back to a string
+// }
+
+// function solution(my_string) {
+//   var answer = ''; // Initialize the answer string
+
+//   for (var i = my_string.length - 1; i >= 0; i--) { // Start from the last character
+//     answer += my_string[i]; // Add the current character to the answer
+//   }
+
+//   return answer; // Return the reversed string
+// }
+
+// function solution(num_list) {
+//   var answer = [0, 0];
+//   for (let i = 0; i < num_list.length; i++) {
+//     if (num_list[i] % 2 === 0) answer[0]++;
+//     if (num_list[i] % 2 === 1) answer[1]++;
+//   }
+//   return answer;
+// }
+
+// console.log(solution([12, 77, 38, 41, 53, 92, 85])); // [2, 5]
+
+// function solution(slice, n) {
+//   var answer = Math.ceil(n / slice);
+//   return answer;
+// }
+
+// console.log(solution(7, 10));
+
+// function solution(array, n) {
+//   var answer = 0;
+
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] === n) answer++;
+//   }
+
+//   return answer;
+// }
+
+// function solution(array, height) {
+//   var answer = 0;
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] >= height) answer++;
+//   }
+//   return answer;
+// }
+
+// function solution(hp) {
+//   var answer = 0;
+//   const generalAnt = 5;
+//   const soldierAnt = 3;
+//   const workerAnt = 1;
+
+//   while (hp > 0) {
+//     if (hp >= generalAnt) {
+//       hp -= generalAnt;
+//       answer++;
+//     } else if (hp >= soldierAnt) {
+//       hp -= soldierAnt;
+//       answer++;
+//     } else if (hp >= workerAnt) {
+//       hp -= workerAnt;
+//       answer++;
+//     }
+//   }
+//   return answer;
+// }
+
+// function event(x) {
+//   const employee = {
+//     name: 'John',
+//     position: x,
+//   };
+
+//   if (!employee.position) {
+//     return '포지션이 없습니다. 포지션을 입력해주세요.';
+//   }
+
+//   return `포지션은 ${employee.position} 입니다.`;
+// }
+
+// console.log(event('developer'));
+
+const employee = {
+  name: 'Eric',
+  equimentTraining: true,
 };
 
-const book = {
-  author: 'Joe Morgan',
-  title: 'Simplifying JavaScript',
-};
+function listCerts(employee) {
+  if (employee.equimentTraining) {
+    employee.certificate = ['Equipment'];
 
-function addBookDefaults(book, defaults) {
-  const fields = Object.keys(defaults);
-  // const bookFields = Object.keys(book);
-  // console.log(fields);
-  // console.log(bookFields);
-
-  const updated = {};
-
-  for (let i = 0; i < fields.length; i++) {
-    const field = fields[i];
-    updated[field] = book[field] || defaults[field];
+    delete employee.equimentTraining;
   }
+
 }
 
-addBookDefaults(book, defaults);
+function checkAuthorization() {
+  if (!employee.equimentTraining) {
+    return 'Not Authorized';
+  }
 
-let filters = new Map(
-  [
-    ['author', ''],
-    ['title', ''],
-    ['year', 2017],
-    ['rating', null],
-  ]
-);
+  return `hi ${employee.name}`;
+}
 
-console.log(filters);r
+listCerts(employee);
+console.log(checkAuthorization(employee));
