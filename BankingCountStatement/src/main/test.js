b501bd8c26a29571799e982689433002f967f5f0
dishin <dishin@maitzsoft.co.kr>
2024-11-29 17:58:01 +0900

    Algorithm

--- BankingCountStatement/src/main/test.js
+++ BankingCountStatement/src/main/test.js
@@ -359,27 +359,493 @@
 
 // console.log(event('developer'));
 
-const employee = {
-  name: 'Eric',
-  equimentTraining: true,
-};
+// const employee = {
+//   name: 'Eric',
+//   equimentTraining: true,
+// };
 
-function listCerts(employee) {
-  if (employee.equimentTraining) {
-    employee.certificate = ['Equipment'];
+// function listCerts(employee) {
+//   if (employee.equimentTraining) {
+//     employee.certificate = ['Equipment'];
 
-    delete employee.equimentTraining;
+//     delete employee.equimentTraining;
+//   }
+
+// }
+
+// function checkAuthorization() {
+//   if (!employee.equimentTraining) {
+//     return 'Not Authorized';
+//   }
+
+//   return `hi ${employee.name}`;
+// }
+
+// listCerts(employee);
+// console.log(checkAuthorization(employee));
+
+// const userConfig = {
+//   images: [],
+// }
+
+// function getImg(userConfig) {
+//   let img = 'default.png';
+//   if (userConfig.images) {
+//     img = userConfig.images[0];
+//   }
+//   return img;
+// }
+// const img = getImg(userConfig);
+// console.log(img);
+
+// function capitalize(name) {
+//   return name[0].toUpperCase() + name.slice(1);
+// }
+
+// console.log(capitalize('john'));
+
+// function greet(name, callback) {
+//   const capitalizedName = callback(name);
+//   console.log(`Hello ${capitalizedName}`);
+// }
+
+// greet('john', capitalize);
+
+// const prices = ['1.0', '2.15'];
+
+// const formattedPrices = [];
+
+// for (let i = 0; i < prices.length; i++) {
+//   formattedPrices.push(parseFloat(prices[i]));
+// }
+
+// console.log(prices);
+
+// console.log(formattedPrices);
+
+// const prices = ['1,0', 'negotiable', '2.15'];
+
+// const formattedPrices = [];
+
+// for (let i = 0; i < prices.length; i++) {
+//   const price = parseFloat(prices[i]);
+//   if (price) {
+//     formattedPrices.push(price);
+//   }
+// }
+
+// console.log(prices);
+
+// console.log(formattedPrices);
+
+const team = [
+  {
+    name: 'melinda',
+    position: 'developer',
+    address : {
+      city: 'seoul',
+      country: 'korea',
+    }
+  },
+  {
+    name: 'john',
+    position: 'designer',
+  },
+  {
+    name: 'katy',
+    position: 'manager',
+  },
+  {
+    name: 'bob',
+    position: 'developer',
+  },
+  {
+    name: 'anna',
+    position: 'designer',
+  }
+];
+
+const copy = [];
+
+// for (let i = 0; i < team.length; i++) {
+//   const member = team[i];
+//   if (member.position === 'developer') {
+//     copy.push(member);
+//   }
+// }
+
+// for (let i = 0; i < team.length; i++) {
+//   const member = team[i];
+//   if (member.position === 'developer') {
+//     const memberCopy = {...member};
+//     copy.push(memberCopy);
+//   }
+// }
+
+// for (let i = 0; i < team.length; i++) {
+//   const member = team[i];
+//   if (member.position === 'developer') {
+//     copy.push(Object.assign({}, member));
+//   }
+// }
+
+// copy[0].name = 'honggildong';
+// copy[0].address.city = 'busan';
+
+// console.log(team);
+
+// console.log(copy);
+
+// function solution(array) {
+//   var answer = 0;
+//   const meddleArray = array.sort((a, b) => a - b);
+//   answer = meddleArray[Math.floor(meddleArray.length / 2)];
+//   return answer;
+// }
+
+// console.log(solution([2, 3, 1, 5, 4])); // 3
+
+const band = [
+  {
+    name: 'corbett',
+    instrument: 'guitar',
+  },
+  {
+    name: 'evan',
+    instrument: 'guitar',
+  },
+  {
+    name: 'sean',
+    instrument: 'bass',
+  },
+  {
+    name: 'brett',
+    instrument: 'drums',
+  },
+];
+
+// const instruments = [];
+
+// for (let i = 0; i < band.length; i++) {
+//   const instrument = band[i].instrument;
+//   instruments.push(instrument);
+// }
+
+const getInstument = (member => member.instrument);
+
+// for (let i = 0; i < band.length; i++) {
+//   instruments.push(getInstument(band[i]));
+// }
+
+const instruments = band.map(getInstument);
+
+console.log(instruments);
+
+// function solution(n) {
+//   var answer = [];
+//   for (let i = 0; i < n; i++) {
+//     if (i % 2 === 1) {
+//       answer.push(i);
+//     }
+//   }
+//   return answer;
+// }
+
+// console.log(solution(10));
+
+// function solution(n) {
+//   var answer = 0;
+//   return answer = Math.ceil(n / 7);
+// }
+
+// console.log(solution(15));
+
+// function solution(price) {
+//   var answer = 0;
+//   if (price < 100000) answer = price;
+//   if (price >= 100000) answer = Math.floor(price * 0.95);
+//   if (price >= 300000) answer = Math.floor(price * 0.90);
+//   if (price >= 500000) answer = Math.floor(price * 0.80);
+//   return answer;
+// }
+
+// console.log(solution(100010));
+
+// function solution(money) {
+//   var answer = [0, 0];
+//   while (money > 0) {
+//     if (money >= 5500) {
+//       answer[1] = money - 5500;
+//       money = money - 5500;
+//       answer[0]++;
+//     } else {
+//       answer[1] = money;
+//       break;
+//     }
+//   }
+//   return answer;
+// }
+
+// console.log(solution(15000));
+
+
+// const instructorsD = [
+//   {
+//     name: '짐',
+//     libraries: ['미디어교육정보 도서관'],
+//   },
+//   {
+//     name: '새라',
+//     libraries: ['기념 도서관', '대학 도서관'],
+//   },
+//   {
+//     name: '엘리엇',
+//     libraries: ['중앙 도서관'],
+//   }
+// ];
+
+// let memorialInstructor = null;
+
+// for (let i = 0; i < instructors.length; i++) {
+//   if (instructors[i].libraries.includes('기념 도서관')) {
+//     memorialInstructor = instructors[i].name;
+//   }
+// }
+
+// const librarian = instructors.find(instructors => instructors.libraries.includes('우주 도서관')) || {librarian: '없음'};
+
+// console.log(librarian);
+
+// const findByLibrary = library => instructors => instructors.libraries.includes(library);
+
+// const librarian = instructorsD.find(findByLibrary('기념 도서관'));
+
+// console.log(librarian);
+
+// function solution(strlist) {
+//   var answer = [];
+//   for (let i = 0; i < strlist.length; i++) {
+//     answer.push(strlist[i].length);
+//   }
+//   return answer;
+// }
+
+// function solution(rsp) {
+//   var answer = '';
+//   var rsp = rsp.split('');
+//   for (let i = 0; i < rsp.length; i++) {
+//     if (rsp[i] == 2) {
+//       answer += '0';
+//     } else if (rsp[i] == 0) {
+//       answer += '5';
+//     } else {
+//       answer += '2';
+//     }
+//   }
+//   return answer;
+// }
+
+// console.log(solution('2'));
+
+// function solution(my_string) {
+//   var answer = '';
+//   var strArr = my_string.split('');
+//   for (let i = 0; i < strArr.length; i++) {
+//     if (strArr[i].charCodeAt() >= 65 && strArr[i].charCodeAt() <= 90) {
+//       answer += strArr[i].toLowerCase();
+//     } else {
+//       answer += strArr[i].toUpperCase();
+//     }
+//   }
+//   return answer;
+// }
+// function solution(arr) {
+//   var answer = [];
+
+//   if (arr.length === 0) {
+//     answer.push(-1);
+//     return answer;
+//   }
+
+//   if (arr.length === 1) {
+//     answer.push(-1);
+//     return answer;
+//   }
+
+//   var sortedArr = [...arr].sort((a, b) => a - b);
+//   var popValue = sortedArr.shift();
+
+//   let smallestRemoved = false;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (popValue !== arr[i] || smallestRemoved) {
+//       if (arr[i] === 10) {
+//         answer.push(-1);
+//       } else {
+//         answer.push(arr[i]);
+//       }
+//     } else {
+//       if (popValue === 10) {
+//         answer.push(-1);
+//       }
+//       smallestRemoved = true;
+//     }
+//   }
+
+//   return answer;
+// }
+// function solution(arr) {
+//   var answer = [];
+
+//   if (arr.length === 0) {
+//     answer.push(-1);
+//     return answer;
+//   }
+
+//   if (arr.length === 1) {
+//     answer.push(-1);
+//     return answer;
+//   }
+
+//   var sortedArr = [...arr].sort((a, b) => a - b);
+//   var popValue = sortedArr.shift();
+
+//   let smallestRemoved = false;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (popValue !== arr[i] || smallestRemoved) {
+
+//         answer.push(arr[i]);
+
+//     } else {
+
+//       smallestRemoved = true;
+//     }
+//   }
+
+//   if (answer.length === 0) {
+//     return [-1];
+//   }
+
+//   return answer;
+// }
+
+// console.log(solution([]));
+
+// function solution(a, b) {
+//   var answer = 0;
+//   for (let i = 0; i < a.length; i++) {
+//     answer += a[i] * b[i];
+//   }
+//   return answer;
+// }
+
+// function solution(arr)
+// {
+//     var answer = [];
+//     let cnt = 0;
+//     while (arr.length > cnt) {
+//         if (arr[cnt] !== arr[cnt + 1]) {
+//             answer.push(arr[cnt]);
+//             cnt++;
+//         } else {
+//             cnt++;
+//         }
+//     }
+//     return answer;
+// }
+
+// console.log(solution([1, 1, 3, 3, 0, 1, 1]));
+
+// function solution(array, commands) {
+//   var answer = [];
+//   for (let i = 0; i < commands.length; i++) {
+//     const start = commands[i][0] - 1;
+//     const end = commands[i][1];
+//     const target = commands[i][2] - 1;
+//     const sliced = array.slice(start, end);
+//     const sorted = sliced.sort((a, b) => a - b);
+//     answer.push(sorted[target]);
+//   }
+//   return answer;
+// }
+
+// function solution(number) {
+//   var answer = 0;
+//   for (let i = 0; i < number.length; i++) {
+//     for (let j = i + 1; j < number.length; j++) {
+//       for (let k = j + 1; k < number.length; k++) {
+//         if (number[i] + number[j] + number[k] === 0) {
+//           answer++;
+//         }
+//       }
+//     }
+//   }
+//   return answer;
+// }
+
+// console.log(solution([-3, -2, -1, 0, 1, 2, 3]));
+const arr = [12, 11, 13, 5, 6];
+
+const bubbleSort = (arr) => {
+  let len = arr.length;
+
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let tmp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = tmp;
+      }
+    }
   }
 
+  return arr;
 }
 
-function checkAuthorization() {
-  if (!employee.equimentTraining) {
-    return 'Not Authorized';
+const insertionSort = (arr) => {
+  for (let i = 1; i < arr.length; i++) {
+    let comparedValue = arr[i];
+    let minIdx = i - 1;
+
+    while (minIdx >= 0) {
+      if (arr[minIdx] > comparedValue) {
+        arr[minIdx + 1] = arr[minIdx];
+        minIdx--;
+      } else {
+        break;
+      }
+    }
+
+    arr[minIdx + 1] = comparedValue;
   }
 
-  return `hi ${employee.name}`;
+  return arr;
 }
 
-listCerts(employee);
-console.log(checkAuthorization(employee));
+console.log(insertionSort(arr));
+
+const binarySearch = (arr, search) => {
+  let lower = 0;
+  let upper = arr.length - 1;
+
+  while (lower <= upper) {
+    let mid = Math.floor((upper + lower) / 2);
+
+    valueMid = arr[mid];
+
+    if (search === valueMid) {
+      return mid + 1;
+    } else if (search < valueMid) {
+      upper = mid - 1;
+    } else if (search > valueMid) {
+      lower = mid + 1;
+    }
+  }
+}
+
+const sortedArr = bubbleSort(arr);
+console.log(sortedArr);
+console.log(binarySearch(sortedArr, 5));
+
+
